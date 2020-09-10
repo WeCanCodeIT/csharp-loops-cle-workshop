5422b09a46ba9c9c42da517158021d677c779878
Jen E <jenevans3ten@gmail.com>
2020-09-10 11:51:53 -0400

    stater code for loops workshop

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace csharp_loops_workshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's Count!");
+            Console.WriteLine("I will count from 1 to the number you enter: ");
+            int countingNumber = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
